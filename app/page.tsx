7153cf2b047b43769ca5a679e273705c68a8a68a
Tilak Mahajan <tilakmahajan1610@gmail.com>
2025-11-20 22:50:42 +0530

    remove the view documention button

--- app/page.tsx
+++ app/page.tsx
@@ -65,12 +65,12 @@ export default function Home() {
             Make a Payment
           </a>
 
-          <a
+          {/* <a
             href="/summary"
             className="px-6 py-3 bg-white border rounded-lg text-gray-700 hover:bg-gray-100 transition"
           >
             View Documentation
-          </a>
+          </a> */}
         </div>
 
       </section>
